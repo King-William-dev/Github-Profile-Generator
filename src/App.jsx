cf453e53f9d74efa6f4b771f1643ac02adab05ec
William Olei <seanapowi@gmail.com>
2024-10-05 06:00:33 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from './components/Logo';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-teal-600">
+      <div className="container text-gray-200 py-3">
+        <Logo />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div>
+      <img
+        src="https://cdn3.iconfinder.com/data/icons/inficons/512/github.png"
+        className="w-24 rounded-full"
+      />
+      <h2>Gihub User Profile</h2>
+    </div>
+  );
+};
+
+export default Logo;
