2c59c5eb07bff0b51f2de25f7af38478194307ea
William Olei <seanapowi@gmail.com>
2024-10-08 18:29:25 +0300

    Add styling

--- src/components/Routes/UserInfo.jsx
+++ src/components/Routes/UserInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const UserInfo = () => {
@@ -26,39 +26,48 @@ const UserInfo = () => {
       </button>
       {user &&
         user?.map((uinfo, id) => (
-          <div
-            key={id}
-            className="flex justify-center md:flex-row md:px-0 px-4 flex-col gap-10"
-          >
-            <img
-              src={uinfo.avatar_url}
-              className="w-[350px] border-2 border-teal-400 md:mx-0 mx-auto rounded-full"
-            />
-            <div className="text-lg px-3 leading-10">
-              <h1 className="text-3xl pb-4">
+          <div key={id} className=" h-screen flex flex-col items-center">
+            <div>
+              <img
+                src={uinfo.avatar_url}
+                className="w-[350px] border-2 border-teal-400 md:mx-0 mx-auto rounded-full"
+              />
+            </div>
+            <div>
+              <h1 className="text-3xl pb-4 mt-5 ">
                 <a href={uinfo.html_url} target="_blank">
                   {uinfo?.name}
                 </a>
               </h1>
-              <h1>
-                <span className="text-teal-400">Login Name</span>:{' '}
-                {uinfo?.login}
+            </div>
+            <h1 className="text-center text-2xl mb-5">
+              <span className="text-purple-800">@{uinfo?.login}</span>
+            </h1>
+            <div className="flex justify-between gap-10">
+              <h1 className=" py-1 px-3">
+                <box-icon type="solid" name="location-plus"></box-icon>
+                <span className=" ml-3 text-2xl">{uinfo?.location}</span>
               </h1>
-              <h1>
-                <span className="text-teal-400">Followers</span>:{' '}
-                {uinfo?.followers}
+              <h1 className="py-1 px-3">
+                <box-icon name="calendar" type="solid"></box-icon>
+                <span className="ml-3 text-2xl">
+                  Joined {new Date(uinfo?.created_at).toLocaleDateString()}
+                </span>
               </h1>
-              <h1>
-                <span className="text-teal-400">Following</span>:{' '}
-                {uinfo?.following}
+            </div>
+            <div className="flex gap-10 ">
+              <h1 className="bg-gray-400 px-8 py-4 mt-10 rounded text-2xl text-center">
+                <span>FOLLOWERS</span>
+                <br /> {uinfo?.followers}
               </h1>
-              <h1>
-                <span className="text-teal-400">Repositories</span>:{' '}
-                {uinfo?.public_repos}
+              <h1 className="bg-gray-400 px-8 py-4 mt-10 rounded text-2xl text-center">
+                <span>FOLLOWING</span>
+                <br /> {uinfo?.following}
               </h1>
-              <h1>
-                <span className="text-teal-400">Joined</span>:{' '}
-                {new Date(uinfo?.created_at).toLocaleDateString()}
+
+              <h1 className="bg-gray-400 px-8 py-4 mt-10 rounded text-2xl text-center">
+                <span>REPOSITORIES</span>
+                <br /> {uinfo?.public_repos}
               </h1>
             </div>
           </div>
--- src/components/UsersContainer.jsx
+++ src/components/UsersContainer.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 const UsersContainer = ({ users }) => {
@@ -8,17 +7,16 @@ const UsersContainer = ({ users }) => {
         users.map((user, id) => (
           <div
             key={id}
-            className="flex w-[200px] border border-gray-500 p-3 flex-col items-center"
+            className="flex w-[250px] border border-gray-500 p-3 flex-col items-center"
           >
             <img
               src={user?.avatar_url}
               className="w-24 mb-4 border-4 rounded-full border-fuchsia-700"
             />
-            <h2 className="text-2xl text-center ">{user?.name}</h2>
-            <h2 className="text-2xl text-center ">{user?.login}</h2>
-            <h2 className="text-xs">{user?.name}</h2>
+            <h2 className="text-2xl text-center mb-4">{user?.login}</h2>
+            <h2 className="text-2xl text-center">{user?.name}</h2>
             <Link to={`/${user?.login}`}>
-              <span className="text-gray-500 bg-teal-500 my-3 font-semibold py-1 px-4 tracking-wide rounded">
+              <span className="text-gray-500 bg-teal-500 font-semibold py-1 px-4 tracking-wide rounded">
                 View Profile
               </span>
             </Link>
