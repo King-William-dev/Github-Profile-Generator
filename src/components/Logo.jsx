f67754e614785e2d8f32af8037b1b8293e719edf
William Olei <seanapowi@gmail.com>
2024-10-08 13:33:30 +0300

    Third commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -7,9 +7,7 @@ const Logo = () => {
         src="https://cdn3.iconfinder.com/data/icons/inficons/512/github.png"
         className="w-24 rounded-full border-4"
       />
-      <h2 className="text-2xl px-2 first-letter:text-5xl">
-        Search Github User Profile
-      </h2>
+      <h2 className="text-2xl px-2">Search Github User Profile</h2>
     </div>
   );
 };
